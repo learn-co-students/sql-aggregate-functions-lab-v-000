797bbc6fe7c489a8c71e47f6e31a8fa2d4ba89ab
github username <github email address>
2015-11-20 16:18:55 -0500

    Done.

--- lib/students.rb
+++ lib/students.rb
@@ -1,24 +1,23 @@
-# name, grade, gpa, tardies
 def highest_student_gpa
-  "SELECT gpa FROM students ORDER BY gpa DESC LIMIT 1;"
+  "SELECT MAX(gpa) AS highest_gpa FROM students;"
 end
 
 def lowest_student_gpa
-  "SELECT gpa FROM students ORDER BY gpa LIMIT 1;"
+  "SELECT MIN(gpa) AS lowest_gpa FROM students;"
 end
 
 def average_student_gpa
-  "SELECT AVG(gpa) FROM students;"
+  "SELECT AVG(gpa) AS average_gpa FROM students;"
 end
 
 def total_tardies_for_all_students
-  "SELECT SUM(tardies) FROM students;"
+  "SELECT SUM(tardies) AS total_tardies FROM students;"
 end
 
 def counts_all_students_groups_by_grade
   "SELECT grade, COUNT(*) FROM student GROUP BY grade;"
 end
 
-def average_student_gpa_for_9th_grade
+def average_gpa_for_9th_grade
   "SELECT AVG(gpa) FROM students WHERE grade = 9;"
 end

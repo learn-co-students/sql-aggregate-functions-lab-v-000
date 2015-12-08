8c70b599bbbf3cc38dcf199fd82fcc22bd9aecd4
Tracy <tracetrace@gmail.com>
2015-12-08 00:05:49 +0000

    Done.

--- lib/students.rb
+++ lib/students.rb
@@ -1,17 +1,19 @@
 def highest_student_gpa
+  "SELECT MAX(gpa) FROM  students;"
 end
 
 def lowest_student_gpa
+  "SELECT MIN(gpa) FROM students;"
 end
 
 def average_student_gpa
+  "SELECT AVG(gpa) FROM students;"
 end
 
 def total_tardies_for_all_students
+  "SELECT SUM(tardies) FROM students;"
 end
 
-def counts_all_students_groups_by_grade
-end
-
-def average_gpa_for_9th_grade
+def average_student_gpa_for_9th_grade
+  "SELECT AVG(gpa) FROM students WHERE grade = 9;"
 end

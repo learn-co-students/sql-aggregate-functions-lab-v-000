da94aafa79a3a777e06456b5eb520e101502a3d3
Richard Shen <rich.shen@nyu.edu>
2015-12-31 04:32:11 +0000

    added aggregate functions

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,8 @@
-
+insert into students (name, grade, gpa, tardies) values ("John", 11, 3.4, 3);
+insert into students (name, grade, gpa, tardies) values ("Koreem", 10, 2.4, 12);
+insert into students (name, grade, gpa, tardies) values ("Zok", 12, 4, 0);
+insert into students (name, grade, gpa, tardies) values ("Kim", 11, 2, 9);
+insert into students (name, grade, gpa, tardies) values ("Bob", 10, 1.9, 17);
+insert into students (name, grade, gpa, tardies) values ("Stef", 9, 2.5, 2);
+insert into students (name, grade, gpa, tardies) values ("George", 9, 3.9, 1);
+insert into students (name, grade, gpa, tardies) values ("Doryl", 9, 3.5, 5);

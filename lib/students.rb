1f5a1583c680a81a27e7e765b25d27cf34ea61ce
Karuna <imkaruna@gmail.com>
2016-01-03 07:00:52 +0000

    Aggregate SQLite3

--- lib/students.rb
+++ lib/students.rb
@@ -1,14 +1,19 @@
 def highest_student_gpa
+  "SELECT gpa FROM students ORDER BY gpa DESC LIMIT 1"
 end
 
 def lowest_student_gpa
+  "SELECT gpa FROM students ORDER BY gpa ASC LIMIT 1"
 end
 
 def average_student_gpa
+  "SELECT AVG(gpa) FROM students"
 end
 
 def total_tardies_for_all_students
+  "SELECT SUM(tardies) FROM students"
 end
 
 def average_gpa_for_9th_grade
+  "SELECT AVG(gpa) FROM students WHERE grade = 9"
 end

bc7d422a3418a1c3bb30cf89941813edc18a7d4d
Ben C <bcwong1@ualberta.ca>
2016-01-05 03:24:05 +0000

    Done.

--- lib/students.rb
+++ lib/students.rb
@@ -1,14 +1,19 @@
 def highest_student_gpa
+  "SELECT MAX(gpa) FROM students;"
 end
 
 def lowest_student_gpa
+  "SELECT MIN(gpa) FROM students;"
 end
 
 def average_student_gpa
+  "SELECT AVG(gpa) FROM students;"
 end
 
 def total_tardies_for_all_students
+  "SELECT SUM(tardies) FROM students;"
 end
 
 def average_gpa_for_9th_grade
+  "SELECT AVG(gpa) FROM STUDENTS WHERE grade == 9;"
 end

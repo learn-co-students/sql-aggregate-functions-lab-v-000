c4104f6587b0f139924a1570d16f8be8707095b1
alexbarron <alexbarron@gmail.com>
2016-01-31 19:52:50 -0800

    Done.

--- lib/students.rb
+++ lib/students.rb
@@ -1,14 +1,19 @@
 def highest_student_gpa
+  "SELECT MAX(gpa) FROM students LIMIT 1;"
 end
 
 def lowest_student_gpa
+  "SELECT MIN(gpa) FROM students LIMIT 1;"
 end
 
 def average_student_gpa
+  "SELECT AVG(gpa) FROM students;"
 end
 
 def total_tardies_for_all_students
+  "SELECT SUM(tardies) FROM students;"
 end
 
 def average_gpa_for_9th_grade
+  "SELECT AVG(gpa) FROM students WHERE grade = 9;"
 end

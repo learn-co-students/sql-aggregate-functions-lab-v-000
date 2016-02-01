fb8853bece1932913c8388262ed2eee578028c91
authorbeard <msnolan@gmail.com>
2016-02-01 16:06:25 -0500

    added missing semicolon to SQL calls

--- lib/students.rb
+++ lib/students.rb
@@ -1,17 +1,17 @@
 def highest_student_gpa
-  "SELECT MAX(gpa) FROM students"
+  "SELECT MAX(gpa) FROM students;"
 end
 
 def lowest_student_gpa
-  "SELECT MIN(gpa) FROM students"
+  "SELECT MIN(gpa) FROM students;"
 end
 
 def average_student_gpa
-  "SELECT AVG(gpa) FROM students"
+  "SELECT AVG(gpa) FROM students;"
 end
 
 def total_tardies_for_all_students
-  "SELECT SUM(tardies) FROM students"
+  "SELECT SUM(tardies) FROM students;"
 end
 
 def average_gpa_for_9th_grade

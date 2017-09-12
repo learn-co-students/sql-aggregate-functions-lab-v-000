79c41b09d72441ab9f7483b7a02c5300fa560ec7
github username <github email address>
2017-09-12 01:43:04 -0700

    Done.

--- lib/students.rb
+++ lib/students.rb
@@ -1,16 +1,25 @@
-## Code your solution below. Note that your SQL queries should be in quotation marks. 
+## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
+  "SELECT MAX(gpa) FROM students;"
 end
 
 def lowest_student_gpa
+  "SELECT MIN(gpa) FROM students;"
 end
 
 def average_student_gpa
+  "SELECT AVG(gpa) FROM students;"
 end
 
 def total_tardies_for_all_students
+  "SELECT SUM(tardies) FROM students;"
 end
 
 def average_gpa_for_9th_grade
+  "SELECT AVG(gpa) FROM students WHERE grade = 9;"
 end
+
+# SELECT COUNT(*) FROM cats WHERE net_worth > 1000000;
+# SELECT AVG(net_worth) FROM cats;
+# SELECT COUNT(owner_id) FROM cats WHERE owner_id = 1;

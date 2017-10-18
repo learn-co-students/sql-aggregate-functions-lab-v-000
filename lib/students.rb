2cf337727a68d194342379e60d504d39dce67d3e
roitblatari <roitblatari@gmail.com>
2017-10-18 12:45:05 -0400

    Done.

--- lib/students.rb
+++ lib/students.rb
@@ -13,7 +13,7 @@ def average_student_gpa
 end
 
 def total_tardies_for_all_students
-  " SELECT sum(tardies) FROM students;" 
+  " SELECT sum(tardies) AS total_tardies FROM students;" 
 end
 
 def average_gpa_for_9th_grade

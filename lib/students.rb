9bcbc1aa625e2c517d0cda88a396eb7684a97366
rh24 <ms.rebeccahong@gmail.com>
2018-01-16 14:41:27 -0500

    Refactor SQL queries to include aliasing return value

--- lib/students.rb
+++ lib/students.rb
@@ -1,11 +1,11 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
-  'SELECT MAX(gpa) FROM students;'
+  'SELECT MAX(gpa) AS highest_student_gpa FROM students;'
 end
 
 def lowest_student_gpa
-  'SELECT MIN(gpa) FROM students;'
+  'SELECT MIN(gpa) AS lowest_student_gpa FROM students;'
 end
 
 def average_student_gpa
@@ -13,9 +13,9 @@ def average_student_gpa
 end
 
 def total_tardies_for_all_students
-  'SELECT SUM(tardies) FROM students;'
+  'SELECT SUM(tardies) AS total_tardies_for_all_students FROM students;'
 end
 
 def average_gpa_for_9th_grade
-  'SELECT AVG(gpa) FROM students WHERE grade = 9;'
+  'SELECT AVG(gpa) FROM students WHERE grade == 9;'
 end

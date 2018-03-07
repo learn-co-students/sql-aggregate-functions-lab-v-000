1780f82bcf29bc5042ad0f616fcfb24a82280a91
smithwebtek <brad@smithwebtek.com>
2018-03-07 09:07:59 -0500

    Add create table for dogs

--- lib/create.sql
+++ lib/create.sql
@@ -5,3 +5,10 @@ CREATE TABLE students
     gpa FLOAT,
     tardies INTEGER);
 
+CREATE TABLE dogs
+    (id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    fleas FLOAT,
+    breed TEXT);
+

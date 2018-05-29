ffcd75a1482a1460bef085d306f9636aeecc6f0c
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-05-29 17:19:58 +0000

    Done.

--- lib/students.rb
+++ lib/students.rb
@@ -19,3 +19,25 @@ end
 def average_gpa_for_9th_grade
   "SELECT AVG(gpa) FROM students WHERE grade = 9;"
 end
+
+=begin
+def highest_student_gpa
+  "SELECT MAX(gpa) as highest_gpa FROM students;"
+end
+
+def lowest_student_gpa
+  "SELECT MIN(gpa) as lowest_gpa FROM students;"
+end
+
+def average_student_gpa
+  "SELECT AVG(gpa) FROM students;"
+end
+
+def total_tardies_for_all_students
+  "SELECT SUM(tardies) AS total_tardies FROM students;"
+end
+
+def average_gpa_for_9th_grade
+    "SELECT AVG(gpa) FROM students WHERE grade==9;"
+end
+=end
